16fee6bbd9fa37c2396eca397cc481e13645e760
mercurystudio99 <mercurystudio99@gmail.com>
2024-02-11 21:11:03 -0800

    add program service section

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -85,6 +85,80 @@ defineProps({
       </div>
     </section>
 
+    <!-- program service section -->
+    <section class="py-3 py-md-5 position-relative">
+      <div class="position-absolute bg-image w-75 h-75 bottom-0" style="background-image: url('assets/images/bg_program.png'); background-size: contain; background-repeat: no-repeat;"></div>
+      <div class="position-relative">
+
+        <div class="container">
+          <div class="row justify-content-md-center">
+            <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
+              <h2 class="mb-4 display-5 text-center">Corporate program services</h2>
+            </div>
+          </div>
+        </div>
+
+        <div class="container overflow-hidden">
+          <div class="row gx-4 gy-2">
+            <div class="col-6 align-self-center">
+              <div class="border border-1 rounded-4 p-2">
+                <div class="d-flex justify-content-start align-items-center">
+                  <img src="../../public/assets/images/icon_infinite_dark.png" width="50" height="50" />
+                  <span>Unlimited access to offers for all employees and their families</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-6 align-self-center">
+              <div class="border border-1 rounded-4 p-2">
+                <div class="d-flex justify-content-start align-items-center">
+                  <img src="../../public/assets/images/icon_gift_dark.png" width="50" height="50" />
+                  <span>Purchasing and delivering gifts to the employees</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-6 align-self-center">
+              <div class="border border-1 rounded-4 p-2">
+                <div class="d-flex justify-content-start align-items-center">
+                  <img src="../../public/assets/images/icon_wallet_dark.png" width="50" height="50" />
+                  <span>Top up wallet for employees to buy gifts</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-6 align-self-center">
+              <div class="border border-1 rounded-4 p-2">
+                <div class="d-flex justify-content-start align-items-center">
+                  <img src="../../public/assets/images/icon_tool_dark.png" width="50" height="50" />
+                  <span>Customer service to ensure your employees' satisfaction</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-6 align-self-center">
+              <div class="border border-1 rounded-4 p-2">
+                <div class="d-flex justify-content-start align-items-center">
+                  <img src="../../public/assets/images/icon_congrat_dark.png" width="50" height="50" />
+                  <span>Manage internal events</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-6 align-self-center">
+              <div class="border border-1 rounded-4 p-2">
+                <div class="d-flex justify-content-start align-items-center">
+                  <img src="../../public/assets/images/icon_upload_dark.png" width="50" height="50" />
+                  <span>Onboarding brands by the company’s request</span>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
 </div>
 </template>
 
